087eb5693dc891c9e07076fbe33351854a306995
sebastian rossa <160712689+Sebasmiumg@users.noreply.github.com>
2024-02-24 10:47:56 -0600

    Add files via upload

---
+++ EJERCICO 3 DEL CORTO.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int primo;
+	
+for(int i=1;i<=50; i++){
+    if(i%2!=0){
+        primo=i;
+        cout<<"El numero "<<primo<<" es primo"<<endl;
+}
+}
+return  0;
+}
+
+
+//5.	Crea un programa que imprima la serie de números primos del 1 al 50.
